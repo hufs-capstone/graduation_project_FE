203baa353c28b7d40fdadcf5e23a75d905732e2c
SunKyu <leesunkeuy@naver.com>
2020-05-30 18:21:58 +0900

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Container, Row, Col } from 'reactstrap';
+import{BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import AgeContainers from './containers/AgeContainer';
+import AppNavBar from './components/navbar';
+import AgeSexContainer from './containers/AgeSexContainer';
+import Home from './routes/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AgeContainers/>
+       <Container>
+        <Row>
+          <Col>
+          <AppNavBar/>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <div>
+          <AgeSexContainer />
+        </div>
+      </Container>
+      <Container>
+        <Router>
+          <div>
+            <Switch>
+              
+              <Route exact path = "/" component={Home}/>
+              
+              {/*
+              <Route path = "/cal1" component={cal1}/>
+              <Route path = "/cal2" component={cal2}/>
+              <Route path = "/comp" component={comp}/>
+              */}
+
+            </Switch>
+          </div>
+        </Router>
+      </Container>
     </div>
   );
 }
