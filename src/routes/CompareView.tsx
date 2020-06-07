e132f016e043390682d4e5a556682b0ccc306319
wooyo2 <37951340+wooyo2@users.noreply.github.com>
2020-06-07 19:06:14 +0900

    Table seperater

--- src/routes/CompareView.tsx
+++ src/routes/CompareView.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Alert } from 'reactstrap';
 import CompareViewContainer from '../containers/CompareViewContainer';
 import ProductTableContiner1 from '../containers/ProductTableContainer1';
 import ProductTableContiner2 from '../containers/ProductTableContainer2';
 import { Container, Row, Col } from 'reactstrap';
+
 function CompareView({}){
     return (
         <div>
             <CompareViewContainer/>
+            <Alert color="primary">
+            상품 정보는 각 제조사에서 제공하는 그람수를 기준으로 표기하고 있으니 참고 바랍니다.
+            </Alert>
             <Container>
                 <Row xs="1" md="2">
                     <Col>
